7332a511f4ec7734fae612253835c35d08cfe51e
antipol <trine_54@hotmail.com>
2019-11-11 16:29:24 +0100

    updating cors middleware

--- middleware/setCors.js
+++ middleware/setCors.js
@@ -4,7 +4,7 @@ exports.setCors = (req, res, next) => {
     "Access-Control-Allow-Headers",
     "Content-Type, X-Requested-With, Accept, Origin"
   );
-  res.header("Access-Control-Allow-Methods", "POST");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
 
   //use next since header is CORS middleware
   next();
